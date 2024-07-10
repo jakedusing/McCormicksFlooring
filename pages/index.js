90bf73b8ef5d47230c361edc5c4be5fac0dae055
Jake Dusing <dusingjake@gmail.com>
2024-07-10 10:54:34 -0500

    start styling for dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,78 +1,89 @@
 import Head from "next/head";
+import { useState } from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillFacebook, AiFillInstagram } from "react-icons/ai";
 import Image from "next/image";
 import mccormickflooring from "../public/mccormickflooring_nobg.png";
 import lvpIcon from "../public/lvp-icon_orig.png";
-import floorbefore from "../public/floorbefore.jpg";
-import floorbefore1 from "../public/floorbefore1.jpg";
-import floorafter from "../public/floorafter.jpg";
-import floorafter1 from "../public/floorafter1.jpg";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div>
+    <div className={darkMode ? "dark" : ""}>
       <Head>
         <title>McCormick's Flooring</title>
       </Head>
-      <main className="bg-white px-10">
+      <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons">McCormick's Flooring</h1>
+            <h1 className="text-xl font-burtons dark:text-white">
+              McCormick's Flooring
+            </h1>
             <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              <li className="dark:text-white">
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-2xl"
+                />
               </li>
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-200 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
+                  href="mailto:mccormicksflooring@gmail.com"
                 >
-                  Facebook
+                  Contact
                 </a>
               </li>
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-300 md:text-6xl">
               Mike McCormick
             </h2>
-            <h3 className="text-2xl py-2">Contractor</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
-              Licensed contactor for your flooring needs.
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Owner of McCormick's Flooring
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-md mx-auto">
+              Professional flooring and tile installation.
+              <br />
+              Great Workmanship.
+              <br />
+              Affordable prices.
+              <br />
+              Quality work.
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
-            <AiFillFacebook />
-            <AiFillInstagram />
+          <div className="text-5xl flex justify-center gap-12 py-1 text-gray-600 dark:text-gray-300">
+            <a
+              href="https://www.facebook.com/p/McCormicks-flooring-100063481025326/?_rdr"
+              target="_blank"
+            >
+              <AiFillFacebook />
+            </a>
+            <a
+              href="https://www.instagram.com/tolyboi_staytru/"
+              target="_blank"
+            >
+              <AiFillInstagram />
+            </a>
           </div>
-          <div className="relative bg-gradient-to-b from-teal-500 rounded-2xl w-80 h-80 mt-20 mx-auto">
+          <div className="relative bg-gradient-to-b from-teal-500 rounded-2xl w-80 h-80 mt-10 mx-auto dark:bg-gradient-to-b dark:from-gray-300 dark:to-gray-500">
             <Image src={mccormickflooring} layout="fill" objectFit="cover" />
           </div>
         </section>
         <section>
-          <div className="flex flex-col items-center text-center justify-center p-10 mt-5 shadow-lg rounded-xl">
+          <div className="flex flex-col items-center text-center justify-center p-10 mt-10 shadow-lg rounded-xl md:max-w-lg mx-auto">
             <Image src={lvpIcon} width={100} height={100} />
-            <h3 className="text-3xl py-1">Services offered</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">
+            <h3 className="text-3xl py-1 dark:text-white">Services offered</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-300">
               Luxury Vinyl Plank, Hardwood, Laminate, Baseboards, and much more.
+              <br />
+              Professional installation of commercial and residential flooring
+              and tile.
             </p>
           </div>
         </section>
-        <div>
-          <div className="flex flex-row justify-between">
-            <Image src={floorbefore} width={500} height={500} />
-          </div>
-          <div>
-            <Image src={floorbefore1} />
-          </div>
-          <div>
-            <Image src={floorafter} />
-          </div>
-          <div>
-            <Image src={floorafter1} />
-          </div>
-        </div>
+        <div></div>
       </main>
     </div>
   );
